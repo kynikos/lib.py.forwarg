9c5af0d7e9d44fcfbd388055896d1c1832a829b0
Dario Giovannetti <dev@dariogiovannetti.net>
2016-04-05 21:23:27 +0800

    Simplify test temporarily

--- test/test_prefix_chars.py
+++ test/test_prefix_chars.py
@@ -8,14 +8,17 @@ import argparse as _m_argparse
 @pytest.fixture
 def parser():
     # TODO: Compare with argparse
-    parser = _m_forwarg.ArgumentParser(prefix_chars='-+')
-    parser.add_argument('-O', action='store_false')
-    parser.add_argument('+O', action='store_true', dest='OO')
+    parser = _m_forwarg.ArgumentParser(prefix_chars='+')
+    parser.add_argument('+O', action='store_false')
+    parser.add_argument('+P', action='store_true')
     return parser
 
 
 def test1(parser):
-    assert parser.parse_args(shlex.split('-O +O')) == _m_argparse.Namespace(
-                                                            O=False, OO=True)
+    assert parser.parse_args(shlex.split('+O +P')) == _m_argparse.Namespace(
+                                                            O=False, P=True)
 
 # TODO: Add more tests...
+#       Note that mixing different prefix characters is not fully supported
+#       yet, in fact the options are stored in the parser object without the
+#       prefix
