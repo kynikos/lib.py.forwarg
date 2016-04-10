80210090c88c5d50a56f05060343ed5fce3cafa4
Dario Giovannetti <dev@dariogiovannetti.net>
2016-04-10 11:49:41 +0800

    Add tests

--- test/test_parsed_args.py
+++ test/test_parsed_args.py
@@ -7,9 +7,28 @@ import argparse as _m_argparse
 
 def test1():
     parser = _m_forwarg.ArgumentParser()
-    foo = parser.add_argument('--foo')
+    argdef = parser.add_argument('--foo')
     parsedargs = parser.parse_args(shlex.split('--foo bar'))
-    fooholder = parsedargs.argdef_to_argholder[foo]
-    assert parsedargs.splitline == [('--foo', fooholder), ('bar', fooholder)]
+    argholder = parsedargs.argdef_to_argholder[argdef]
+    assert parsedargs.splitline == [('--foo', argholder), ('bar', argholder)]
+    assert argholder.parsed_arg_indices == [0, 1]
+
+
+def test2():
+    parser = _m_forwarg.ArgumentParser()
+    argdef = parser.add_argument('--foo')
+    parsedargs = parser.parse_args(shlex.split('--foo=bar'))
+    argholder = parsedargs.argdef_to_argholder[argdef]
+    assert parsedargs.splitline == [('--foo=bar', argholder)]
+    assert argholder.parsed_arg_indices == [0]
+
+
+def test3():
+    parser = _m_forwarg.ArgumentParser()
+    argdef = parser.add_argument('-o')
+    parsedargs = parser.parse_args(shlex.split('-o=bar'))
+    argholder = parsedargs.argdef_to_argholder[argdef]
+    assert parsedargs.splitline == [[('-', None), ('o=bar', argholder)]]
+    assert argholder.parsed_arg_indices == [(0, 1)]
 
 # TODO: Add more tests...
